4ac6e2ea182c84b2f8ff83028d13b1917d5edde6
CipherGo <battleroyalesigmax96@gmail.com>
2024-08-20 21:02:35 -0500

    Añadiendo musl-1.2.0

---
+++ musl-1.2.0/src/unistd/ftruncate.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include "syscall.h"
+
+int ftruncate(int fd, off_t length)
+{
+	//return syscall(SYS_ftruncate, fd, __SYSCALL_LL_O(length));
+	return syscall(SYS_ftruncate, fd, (int)length);
+}
+
+weak_alias(ftruncate, ftruncate64);
